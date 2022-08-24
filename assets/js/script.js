6dd468b379f3253d7e02fcf1e5748d929b097701
Will W <will.winston@gmail.com>
2022-08-24 15:06:43 -0400

    Fixed search functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
-var movieDBAPI = "ae8cbfc11d012e219d3b44e276a96f51";
-movie = $("#searchmovies"); 
-// console.log(movie);
+var tmdbAPIkey = "ae8cbfc11d012e219d3b44e276a96f51";
+var imdbAPIkey = "k_lomgkwyt";
+var tmdbInput = $("#tmdbInput");
+var imdbInput = $("#imdbInput");
 
-
-$("#searchBtn").on("click",function(event){
+$("#tmdbBtn").on("click",function(event){
     event.preventDefault();
-    // console.log(movie);
+    // console.log(tmdbInput);
     
-    var titleURL = "https://api.themoviedb.org/3/search/movie?api_key=ae8cbfc11d012e219d3b44e276a96f51&language=en-US&page=1&include_adult=false&query=" + movie.value;
-    // console.log(titleURL);
+    var tmdbURL = "https://api.themoviedb.org/3/search/movie?api_key=" + tmdbAPIkey + "&language=en-US&page=1&include_adult=false&query=" + tmdbInput.val();
+    console.log(tmdbURL);
 
-    fetch(titleURL)
+    fetch(tmdbURL)
     .then(function(response){
         response.json().then(function (data) {
             console.log(data);
-            return data;
+            console.log(data.results[0].original_title);
+            console.log(data.results[0].overview);
 
-
-            
-        });
-        
+        }); 
     });
+});
 
+$("#imdbBtn").on("click",function(event){
+    event.preventDefault();
+    // console.log(imdbInput);
+    // console.log(imdbInput.val());
+    
+    var imdbURL = "https://imdb-api.com/en/API/Search/" + imdbAPIkey + "/" + imdbInput.val();
+    console.log(imdbURL);
+
+    fetch(imdbURL)
+    .then(function(response){
+        response.json().then(function (data) {
+            console.log(data);
+            console.log(data.results[0].title);
+            console.log(data.results[0].description);
+        }); 
+    });
 });
 
 
