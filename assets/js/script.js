ed8789d14e509d7a01b8350351b1bff841dc1e9b
Will W <will.winston@gmail.com>
2022-08-23 23:11:33 -0400

    Updated the js file in assets/js and removed js file at the root of the repo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,23 @@
 var movieDBAPI = "ae8cbfc11d012e219d3b44e276a96f51";
-movie= $("#searchmovies"); 
-console.log(movie);
-
-console.log($("#searchmovies"));
+movie = $("#searchmovies"); 
+// console.log(movie);
 
 
-$("#searchmovies").on("click", function(){
-var titleURL = "https://api.themoviedb.org/3/search/movie?api_key=ae8cbfc11d012e219d3b44e276a96f51&language=en-US&page=1&include_adult=false&query=" + "inception";
-fetch(titleURL)
-.then(function(response){
-    response.json().then(function (data) {
-        console.log(data);
-        return data;
-    });
+$("#searchBtn").on("click",function(event){
+    event.preventDefault();
+    // console.log(movie);
     
-});
+    var titleURL = "https://api.themoviedb.org/3/search/movie?api_key=ae8cbfc11d012e219d3b44e276a96f51&language=en-US&page=1&include_adult=false&query=" + movie.value;
+    // console.log(titleURL);
 
-
+    fetch(titleURL)
+    .then(function(response){
+        response.json().then(function (data) {
+            console.log(data);
+            return data;
+        });
+        
+    });
 
 });
 
