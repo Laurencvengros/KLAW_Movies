bd38d4e7dc41c10106097b2f94053f65d5ffbde0
Lauren <l.cvengros@icloud.com>
2022-08-27 20:09:28 -0400

    working on add to favs button (in progress)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,145 @@
 var omdbAPI = "31aa90e4";
-var imdbAPIkey = "k_lomgkwyt";
+var imdbAPIkey = "k_8rkm66bk";
 var titleEl = document.getElementById("#movietitle");
 var searchMovie= $("#searchmovies");
+var favMovies= {
+
+    personalSaves:[]
+};
+console.log(favMovies.personalSaves);
+//favMovies.personalSaves.push(personalSave)
+
+var savedPosters = JSON.parse(localStorage.getItem('favMovies'));
+console.log(savedPosters.personalSaves[0].movieTitle);
+
+
+for(var i = 0; i < savedPosters.personalSaves.length; i++){
+        //var ulEl = document.createElement("h2");
+    var liEl = document.createElement('li');
+    liEl.innerHTML = "<h2>"  + savedPosters.personalSaves[i].movieTitle + "</h2>";
+    $("#favorites").append(liEl);
+    
+    
+
+} 
+
+function getPosters(){
+    var posterAPI = "https://imdb-api.com/en/API/Search/" + imdbAPIkey + "/" + searchMovie;
+    fetch(posterAPI)
+        .then(function(response){
+            return response.json();
+
+            
+            
+        })
+        .then(function(data){
+            var poster = data.results[0].image;
+
+                console.log(poster);
+
+
+            var card = $("<div class='card'>")
+            var TextDiv = $("<div>")
+            imgEl=$("<img>").attr('src', poster).css('height', '500px');
+            TextDiv.append(imgEl)
+            card.append(TextDiv);
+
+            $("#movie-poster").append(card);
+
+
+        });
+};
+
+$("#favBtn").on("click", function(){
+    var personal = {
+        movieTitle: "",
+        poster: "",
+    };
+    
+
+    personal.movieTitle=$("#searchmovies").val();
+    console.log(personal);
+    console.log(favMovies.personalSaves);
+    favMovies.personalSaves.push(personal);
+    localStorage.setItem("favMovies", JSON.stringify(favMovies));
+    console.log(localStorage.getItem("favMovies"));
+
+    var liEl = document.createElement('li');
+    liEl.innerHTML = "<h2>"  + personal.movieTitle + "</h2>";
+    $("#favorites").append(liEl);
+ 
+     
+});
+
+
+
+function getMovie(){
+var movieTitle = "http://www.omdbapi.com/?apikey=31aa90e4&t=" + searchMovie + "&plot=full&r=json" 
+console.log(searchMovie);
+
+
+fetch(movieTitle)
+.then(function(response){
+    return response.json();
+
+    
+    
+})
+.then(function(data){
+    var titleVal = data.Title
+    var yearVal = data.Year
+    var plotVal = data.Plot
+    var ratingVal = data.imdbRating
+  
+console.log(titleVal);
+console.log(yearVal);
+console.log(plotVal);
+console.log(ratingVal );
+
+var titleVal = data.Title
+var yearVal = data.Year
+var plotVal = data.Plot
+var ratingVal = data.imdbRating
+var TitleHeader = $("<h6>");
+var TextDiv = $("<div>");
+var titleEl = $("<p>").text(titleVal + " " + "(" + yearVal + ")");
+var summaryTab =$("<p>").text("Summary: ");
+var plotEl = $("<p>").text( plotVal);
+var ratingEl = $("<p>").text("IMDB Rating: " + ratingVal);
+var highRating =$("<p>").text("IMDB Rating: " + ratingVal + " " + " (Highly Rated!)");
+
+
+
+
+
+
+TitleHeader.append(titleEl );
+TextDiv.append(summaryTab);
+TextDiv.append(plotEl);
+$("#movie-data").append(TextDiv);
+$("#movie-title").append(TitleHeader)
+
+
+
+if(ratingVal > 8){
+TextDiv.prepend(highRating);
+}else{
+    TextDiv.prepend(ratingEl);
+}
+})
+
+};
+
+
+
+
+function clearMovie(){
+    $("#movie-title").empty();
+    $("#movie-poster").empty();
+    $("#movie-data").empty();
+}
+
+
 $("#homeSec").show();
 $("#aboutSec").hide();
 $("#personalizeSec").hide();
@@ -88,99 +226,3 @@ $("#homeBtn").on("click", function(){
     $(".display-poster").css('display', "inline")
     
 });
-
-function getPosters(){
-    var posterAPI = "https://imdb-api.com/en/API/Search/" + imdbAPIkey + "/" + searchMovie;
-    fetch(posterAPI)
-.then(function(response){
-    return response.json();
-
-    
-    
-})
-.then(function(data){
-var poster = data.results[0].image;
-
-console.log(poster);
-
-
-var card = $("<div class='card'>")
-var TextDiv = $("<div>")
-imgEl=$("<img>").attr('src', poster).css('height', '500px');
-TextDiv.append(imgEl)
-card.append(TextDiv);
-$("#movie-poster").append(card);
-
-});
-
-}
-
-
-
-
-function getMovie(){
-var movieTitle = "http://www.omdbapi.com/?apikey=31aa90e4&t=" + searchMovie + "&plot=full&r=json" 
-console.log(searchMovie);
-
-
-fetch(movieTitle)
-.then(function(response){
-    return response.json();
-
-    
-    
-})
-.then(function(data){
-    var titleVal = data.Title
-    var yearVal = data.Year
-    var plotVal = data.Plot
-    var ratingVal = data.imdbRating
-  
-console.log(titleVal);
-console.log(yearVal);
-console.log(plotVal);
-console.log(ratingVal );
-
-var titleVal = data.Title
-var yearVal = data.Year
-var plotVal = data.Plot
-var ratingVal = data.imdbRating
-var TitleHeader = $("<h6>");
-var TextDiv = $("<div>");
-var titleEl = $("<p>").text(titleVal + " " + "(" + yearVal + ")");
-var summaryTab =$("<p>").text("Summary: ");
-var plotEl = $("<p>").text( plotVal);
-var ratingEl = $("<p>").text("IMDB Rating: " + ratingVal);
-var highRating =$("<p>").text("IMDB Rating: " + ratingVal + " " + " (Highly Rated!)");
-
-
-
-
-
-
-TitleHeader.append(titleEl );
-TextDiv.append(summaryTab);
-TextDiv.append(plotEl);
-$("#movie-data").append(TextDiv);
-$("#movie-title").append(TitleHeader)
-
-
-
-if(ratingVal > 8){
-TextDiv.prepend(highRating);
-}else{
-    TextDiv.prepend(ratingEl);
-}
-})
-
-};
-
-
-
-
-function clearMovie(){
-    $("#movie-title").empty();
-    $("#movie-poster").empty();
-    $("#movie-data").empty();
-}
-
