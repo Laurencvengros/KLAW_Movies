17c0f58095b6d586e77f7c2e210270d34334cf67
Will W <will.winston@gmail.com>
2022-08-29 15:21:09 -0400

    Built upon the ideas for having a personalized page and created separate searches for movies and a spot to save recent searches

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,29 +3,35 @@
 // Case 2 - User has refreshed the page and is adding more favorites
 // Case 3 - User has added maximum number of favorites
 
-var omdbAPI = "31aa90e4";
+var omdbAPIkey = "31aa90e4";
 var imdbAPIkey = "k_lomgkwyt";
 var titleEl = document.getElementById("#movietitle");
-var searchMovie= $("#searchmovies");
+var searchMovie = "";
+var personalMainImage = $("#personalMainImg");
 
 let favMovies= { personalSaves:[] };
 let moveTitleGlobal = "";
 let posterGlobal = "";
-
+let pboxIndex = 0;
 
 //After a refresh check to see if the local storage has values
 let savedPosters = JSON.parse(localStorage.getItem('favMovies'));
+
+//Hide add favorites
+$("#favBtn").hide();
+
 if(savedPosters != null){
     console.log("Loading posters");
     favMovies = savedPosters;
-    $("#favorites").empty();
+    // $("#favorites").empty();
+    
+    $("#pbox" + pboxIndex).empty();
     
     //Add items from local storage to personalized page
-    for(var i = 0; i < savedPosters.personalSaves.length; i++){
-
-        var liEl = document.createElement('li');
-        liEl.innerHTML = `<img src="`  + savedPosters.personalSaves[i].poster + `">`;
-        $("#favorites").append(liEl);            
+    for(var i = 0; i < savedPosters.personalSaves.length; i++){    
+        $("#pbox" + pboxIndex).empty();
+        $("#pbox" + pboxIndex).append(`<img src="`  + savedPosters.personalSaves[i].poster + `" style="max-width:100%;">`);
+        pboxIndex++;    
     } 
 }
 
@@ -33,12 +39,43 @@ if(savedPosters != null){
 $("#searchBtn").on("click", async function(event){
     event.stopPropagation();
 
-    searchMovie = $("#searchmovies").val();
-    $(".display-poster").css("display", "none")
+    if ($("#searchmovies").val()) {
+        searchMovie = $("#searchmovies").val();
+        console.log("Here")
+    }
+    else if ($("#selectVal").val() != "Recent Searches"){
+        searchMovie = $("#selectVal").val();
+    }
+    else {
+        console.log("Error Nothing selected for search")
+    }
 
-    clearMovie()
-    getMovie();
-    getPosters();
+    if (searchMovie) {
+        $("#favBtn").show();
+        $(".display-poster").css("display", "none");
+
+        var card = $("<div class='card'>");
+        var TextDiv = $("<div>");
+        var imgEl=$("<img>").attr('src',"./assets/images/loading.gif").css('height', '500px');
+        TextDiv.append(imgEl);
+        card.append(TextDiv);
+
+        if ($("input[type='radio']:checked").attr("id") === "imdbSearch"){
+            clearMovie();
+            getIMDBmovies();
+        }
+        else if($("input[type='radio']:checked").attr("id") === "omdbSearch"){
+            clearMovie();
+            getOMDBmovies();
+        }
+        else if ($("input[type='radio']:checked").attr("id") === "tmdbSearch"){
+            clearMovie();
+            getTMDBmovies();
+        }
+        else{
+            console.log("Error with gathering API search info from search form");
+        }
+    }
 });
 
 //Add movies to the personalized page
@@ -48,11 +85,7 @@ $("#favBtn").on("click", function(event){
     personal = { movieTitle: "", poster: "" };
     personal.movieTitle = moveTitleGlobal;
     personal.poster = posterGlobal;
-
     var notSaved = true;
-    var liEl = document.createElement('li');
-    liEl.innerHTML =  `<img src="`  + personal.poster + `">`;
-    $("#favorites").append(liEl);
 
     //Don't add items if they exist on the personalized page
     for (let i = 0; i < favMovies.personalSaves.length; i++) {
@@ -65,24 +98,75 @@ $("#favBtn").on("click", function(event){
 
     //Save new items to local storage and place in memory
     if (notSaved){
-        favMovies.personalSaves.push(personal);
-        localStorage.setItem("favMovies", JSON.stringify(favMovies));
+
+        if (pboxIndex === 8) {
+            console.log("save after pbox 8 -- pbox is " + pboxIndex)
+            favMovies.personalSaves.shift();
+            favMovies.personalSaves.push(personal);
+            localStorage.setItem("favMovies", JSON.stringify(favMovies));
+            
+            for (let i = 0; i < favMovies.personalSaves.length; i++){
+                $("#pbox" + pboxIndex).empty();
+                $("#pbox" + pboxIndex).append(`<img src="`  + favMovies.personalSaves[i].poster + `" style="max-width:100%;">`);
+            }
+           
+        }
+        else{
+            console.log("save before pbox 8 -- pbox is " + pboxIndex)
+            favMovies.personalSaves.push(personal);
+            localStorage.setItem("favMovies", JSON.stringify(favMovies));
+            $("#pbox" + pboxIndex).empty();
+            $("#pbox" + pboxIndex).append(`<img src="`  + personal.poster + `" style="max-width:100%;">`);
+            pboxIndex++;
+        }
+        
     }
 
     return true;
 });
 
-//API call for the posters that also adds items to the home page
-async function getPosters(){
-    var posterAPI = "https://imdb-api.com/en/API/Search/" + imdbAPIkey + "/" + searchMovie;
+for (let i = 0; i < 8; i++) {
+    $("#pbox" + i).on("click", function(event){
+        event.stopPropagation();
+        // console.log(this.innerHTML);
+        personalMainImage.empty();
+        personalMainImage.append(this.innerHTML);
+    
+    });
+}
 
-    await fetch(posterAPI)
+//API call for the posters that also adds items to the home page
+async function getIMDBmovies(){
+    
+    var imdbAPI = "https://imdb-api.com/API/AdvancedSearch/" + imdbAPIkey + "/?title=" + searchMovie;
+    // console.log(imdbAPI);
+        await fetch(imdbAPI)
         .then(function(response){
             return response.json();       
             
         })
         .then(function(data){
+            console.log(data);
             var poster = data.results[0].image;
+            var titleVal = data.results[0].title;        
+            var yearVal = data.results[0].description;
+            var plotVal = data.results[0].plot;
+            var ratingVal = data.results[0].imDbRating;
+            
+            var TitleHeader = $("<h6>");
+            var TextDiv = $("<div>");
+            var titleEl = $("<p>").text(titleVal + " " + "(" + yearVal + ")");
+            var summaryTab =$("<p>").text("Summary: ");
+            var plotEl = $("<p>").text( plotVal);
+            var ratingEl = $("<p>").text("IMDB Rating: " + ratingVal);
+            var highRating =$("<p>").text("IMDB Rating: " + ratingVal + " " + " (Highly Rated!)");
+
+            TitleHeader.append(titleEl);
+            TextDiv.append(summaryTab);
+            TextDiv.append(plotEl);
+            $("#movie-data").append(TextDiv);
+            $("#movie-title").append(TitleHeader);
+
             var card = $("<div class='card'>")
             var TextDiv = $("<div>")
             imgEl=$("<img>").attr('src', poster).css('height', '500px');
@@ -90,7 +174,71 @@ async function getPosters(){
             card.append(TextDiv);
 
             $("#movie-poster").append(card);
-            posterGlobal = data.results[0].image;
+
+            if (ratingVal > 8){
+                TextDiv.prepend(highRating);
+            }
+            else {
+                TextDiv.prepend(ratingEl);
+            }
+            
+            posterGlobal = poster;
+            moveTitleGlobal = titleVal;
+            console.log(moveTitleGlobal + " " + posterGlobal);
+
+           
+            return true;
+        });
+
+    return true;
+};
+
+async function getTMDBmovies(){
+    var tmDBAPI = `https://api.themoviedb.org/3/search/movie?api_key=ae8cbfc11d012e219d3b44e276a96f51&language=en-US&page=1&include_adult=false&query="` + searchMovie + `"`;
+
+    await fetch(tmDBAPI)
+        .then(function(response){
+            return response.json();       
+            
+        })
+        .then(function(data){
+            var poster = "https://image.tmdb.org/t/p/w500" + data.results[0].poster_path;
+            var titleVal = data.results[0].title;        
+            var yearVal = data.results[0].release_date;
+            var plotVal = data.results[0].overview;
+            var ratingVal = data.results[0].vote_average;
+            
+            var TitleHeader = $("<h6>");
+            var TextDiv = $("<div>");
+            var titleEl = $("<p>").text(titleVal + " " + "(" + yearVal + ")");
+            var summaryTab =$("<p>").text("Summary: ");
+            var plotEl = $("<p>").text( plotVal);
+            var ratingEl = $("<p>").text("IMDB Rating: " + ratingVal);
+            var highRating =$("<p>").text("IMDB Rating: " + ratingVal + " " + " (Highly Rated!)");
+
+            TitleHeader.append(titleEl);
+            TextDiv.append(summaryTab);
+            TextDiv.append(plotEl);
+            $("#movie-data").append(TextDiv);
+            $("#movie-title").append(TitleHeader);
+
+            var card = $("<div class='card'>")
+            var TextDiv = $("<div>")
+            imgEl=$("<img>").attr('src', poster).css('height', '500px');
+            TextDiv.append(imgEl)
+            card.append(TextDiv);
+
+            $("#movie-poster").append(card);
+
+            if (ratingVal > 8){
+                TextDiv.prepend(highRating);
+            }
+            else {
+                TextDiv.prepend(ratingEl);
+            }
+            
+            posterGlobal = poster;
+            moveTitleGlobal = titleVal;
            
             return true;
         });
@@ -99,16 +247,17 @@ async function getPosters(){
 };
 
 //API call for movie details that also adds items to the home pages
-async function getMovie(){
-    var movieTitle = "http://www.omdbapi.com/?apikey=31aa90e4&t=" + searchMovie + "&plot=full&r=json";
-
-    await fetch(movieTitle)
+async function getOMDBmovies(){
+    var omdbAPI = "http://www.omdbapi.com/?apikey=31aa90e4&t=" + searchMovie + "&plot=full&r=json";
+    console.log(omdbAPI);
+    await fetch(omdbAPI)
         .then(function(response){
         return response.json();
 
     })
     
     .then(function(data){
+        var poster = data.Poster;
         var titleVal = data.Title;        
         var yearVal = data.Year;
         var plotVal = data.Plot;
@@ -117,6 +266,7 @@ async function getMovie(){
         var yearVal = data.Year;
         var plotVal = data.Plot;
         var ratingVal = data.imdbRating;
+
         var TitleHeader = $("<h6>");
         var TextDiv = $("<div>");
         var titleEl = $("<p>").text(titleVal + " " + "(" + yearVal + ")");
@@ -125,20 +275,28 @@ async function getMovie(){
         var ratingEl = $("<p>").text("IMDB Rating: " + ratingVal);
         var highRating =$("<p>").text("IMDB Rating: " + ratingVal + " " + " (Highly Rated!)");
 
-        moveTitleGlobal = data.Title;
+        var card = $("<div class='card'>")
+        var TextDiv = $("<div>")
+        imgEl=$("<img>").attr('src', poster).css('height', '500px');
+        TextDiv.append(imgEl)
+        card.append(TextDiv);
+        $("#movie-poster").append(card);
 
         TitleHeader.append(titleEl);
         TextDiv.append(summaryTab);
         TextDiv.append(plotEl);
         $("#movie-data").append(TextDiv);
         $("#movie-title").append(TitleHeader);
-
+           
         if (ratingVal > 8){
             TextDiv.prepend(highRating);
         }
         else {
             TextDiv.prepend(ratingEl);
         }
+
+        posterGlobal = poster;
+        moveTitleGlobal = titleVal;
 
         return true;
     })
