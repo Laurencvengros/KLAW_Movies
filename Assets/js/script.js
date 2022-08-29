dcfde8f78b67908fd0fbdf5ce8529f5d3f859690
kelsieszost <owner@kelsieszost.design>
2022-08-29 16:34:13 -0400

    trivia game layout

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,6 +8,28 @@ $("#personalizeSec").hide();
 $("#top20Sec").hide();
 $("#triviaSec").hide();
 
+var carousels = bulmaCarousel.attach('.carousel', options);
+
+// Loop on each carousel initialized
+for(var i = 0; i < carousels.length; i++) {
+	// Add listener to  event
+	carousels[i].on('before:show', state => {
+		console.log(state);
+	});
+}
+
+// Access to bulmaCarousel instance of an element
+var element = document.querySelector('#my-element');
+if (element && element.bulmaCarousel) {
+	// bulmaCarousel instance is available as element.bulmaCarousel
+	element.bulmaCarousel.on('before-show', function(state) {
+		console.log(state);
+	});
+}
+
+$.getJSON("./assets/data/movies.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
 
 $("#homeBtn").on("click", function(){
     $("#homeSec").show();
