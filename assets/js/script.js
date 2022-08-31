0dd6cafc45e2a5c3e42815965979c3dc51758ba1
Will W <will.winston@gmail.com>
2022-08-31 16:12:02 -0400

    Adding the top20 section on the behalf of Andrew

--- assets/js/script.js
+++ assets/js/script.js
@@ -426,3 +426,361 @@ $("#poster5").on("click", function(){
     // getMovie(searchMovie);
 });
 
+//Code for Favorites Section
+
+function favoriteMoviesTMDB(){
+    var tmDBAPI = `https://api.themoviedb.org/3/search/movie?api_key=ae8cbfc11d012e219d3b44e276a96f51&language=en-US&page=1&include_adult=false&query="` + searchMovie + `"`;
+    console.log(tmDBAPI);
+
+    fetch(tmDBAPI)
+        .then(function(response){
+            return response.json();       
+            
+        })
+        .then(function(data){
+            var poster = "https://image.tmdb.org/t/p/w500" + data.results[0].poster_path;
+            var titleVal = data.results[0].title;        
+            var yearVal = data.results[0].release_date;
+            var plotVal = data.results[0].overview;
+            var ratingVal = data.results[0].vote_average;
+            
+            var TitleHeader = $("<h6>");
+            var TextDiv = $("<div>");
+            var titleEl = $("<p>").text(titleVal + " " + "(" + yearVal + ")");
+            var summaryTab =$("<p>").text("Summary: ");
+            var plotEl = $("<p>").text( plotVal);
+            var ratingEl = $("<p>").text("IMDB Rating: " + ratingVal);
+            var highRating =$("<p>").text("IMDB Rating: " + ratingVal + " " + " (Highly Rated!)");
+
+            TitleHeader.append(titleEl);
+            TextDiv.append(summaryTab);
+            TextDiv.append(plotEl);
+            $("#favorite-data").append(TextDiv);
+            $("#favorite-title").append(TitleHeader);
+
+            // var card = $("<div class='card'>")
+            var TextDiv = $("<div>")
+            imgEl=$("<img>").attr('src', poster).css('height', '500px');
+            TextDiv.append(imgEl)
+            // card.append(TextDiv);
+
+            $("#favorite-poster").append(TextDiv);
+
+            if (ratingVal > 8){
+                TextDiv.prepend(highRating);
+            }
+            else {
+                TextDiv.prepend(ratingEl);
+            }
+            
+            posterGlobal = poster;
+            moveTitleGlobal = titleVal;
+           
+            return true;
+        });
+
+    return true;
+};
+
+function favoriteMoviesIMDB(){
+    
+    var imdbAPI = "https://imdb-api.com/en/API/SearchMovie/k_a87jnn16/" + searchMovie;
+    // console.log(imdbAPI);
+        fetch(imdbAPI)
+        .then(function(response){
+            return response.json();       
+            
+        })
+        .then(function(data){
+            console.log(data);
+            var poster = data.results[0].image;
+            var titleVal = data.results[0].title;        
+            var yearVal = data.results[0].description;
+            var plotVal = data.results[0].plot;
+            var ratingVal = data.results[0].imDbRating;
+            
+            var TitleHeader = $("<h6>");
+            var TextDiv = $("<div>");
+            var titleEl = $("<p>").text(titleVal + " " + "(" + yearVal + ")");
+            var summaryTab =$("<p>").text("Summary: ");
+            var plotEl = $("<p>").text( plotVal);
+            var ratingEl = $("<p>").text("IMDB Rating: " + ratingVal);
+            var highRating =$("<p>").text("IMDB Rating: " + ratingVal + " " + " (Highly Rated!)");
+
+            TitleHeader.append(titleEl);
+            TextDiv.append(summaryTab);
+            TextDiv.append(plotEl);
+            $("#favorite-data").append(TextDiv);
+            $("#favorite-title").append(TitleHeader);
+
+            // var card = $("<div class='card'>")
+            var TextDiv = $("<div>")
+            imgEl=$("<img>").attr('src', poster).css('height', '500px');
+            TextDiv.append(imgEl)
+            // card.append(TextDiv);
+
+            $("#favorite-poster").append(TextDiv);
+
+            if (ratingVal > 8){
+                TextDiv.prepend(highRating);
+            }
+            else {
+                TextDiv.prepend(ratingEl);
+            }
+            
+            posterGlobal = poster;
+            moveTitleGlobal = titleVal;
+            console.log(moveTitleGlobal + " " + posterGlobal);
+
+           
+            return true;
+        });
+
+    return true;
+};
+
+function clearFavorite(){
+    $("#favorite-title").empty();
+    $("#favorite-poster").empty();
+    $("#favorite-data").empty();
+}
+
+$("#top20Btn").on("click", function(){
+    $("#homeSec").hide();
+    $("#aboutSec").hide();
+    $("#personalizeSec").hide();
+    $("#top20Sec").show();
+    $("#triviaSec").hide();
+    $("#displayFav").hide();
+
+});
+
+$("#kelsie1").on("click", function(){
+    clearFavorite();
+    searchMovie = "Uptown Girls";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#kelsie2").on("click", function(){
+    clearFavorite();
+    searchMovie = "Ella Enchanted";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#kelsie3").on("click", function(){
+    clearFavorite();
+    searchMovie = "New York Minute";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#kelsie4").on("click", function(){
+    clearFavorite();
+    searchMovie = "The Lorax";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#kelsie5").on("click", function(){
+    clearFavorite();
+    searchMovie = "The Social Network";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#lauren1").on("click", function(){
+    clearFavorite();
+    searchMovie = "Spirited Away";
+    favoriteMoviesIMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#lauren2").on("click", function(){
+    clearFavorite();
+    searchMovie = "My Neighbor Totoro";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#lauren3").on("click", function(){
+    clearFavorite();
+    searchMovie = "Your Name";
+    favoriteMoviesIMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#lauren4").on("click", function(){
+    clearFavorite();
+    searchMovie = "Sinister";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#lauren5").on("click", function(){
+    clearFavorite();
+    searchMovie = "As Above So Below";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#drew1").on("click", function(){
+    clearFavorite();
+    searchMovie = "Hitch";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#drew2").on("click", function(){
+    clearFavorite();
+    searchMovie = "The Secret Life of Walter Mitty";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#drew3").on("click", function(){
+    clearFavorite();
+    searchMovie = "The Green Mile";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#drew4").on("click", function(){
+    clearFavorite();
+    searchMovie = "Treasure Planet";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#drew5").on("click", function(){
+    clearFavorite();
+    searchMovie = "Guardians of the Galaxy Vol. 2";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#will1").on("click", function(){
+    clearFavorite();
+    searchMovie = "Back to the Future Part II";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#will2").on("click", function(){
+    clearFavorite();
+    searchMovie = "Terminator 2";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#will3").on("click", function(){
+    clearFavorite();
+    searchMovie = "Forest Gump";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#will4").on("click", function(){
+    clearFavorite();
+    searchMovie = "Memento";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#will5").on("click", function(){
+    clearFavorite();
+    searchMovie = "Black Panther";
+    favoriteMoviesTMDB(searchMovie);
+    $("#kelsieFavs").hide();
+    $("#laurenFavs").hide();
+    $("#drewFavs").hide();
+    $("#willFavs").hide();
+    $("#displayFav").show();
+});
+
+$("#favBack").on("click", function() {
+    clearFavorite();
+    $("#kelsieFavs").show();
+    $("#laurenFavs").show();
+    $("#drewFavs").show();
+    $("#willFavs").show();
+    $("#displayFav").hide();
+});
+
